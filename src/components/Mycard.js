9ebdee22f7980d31e0dbeaf4179488dcdf7882c4
AlexeyKozlovsky <kozaa.19@uni-dubna.ru>
2021-06-19 08:20:55 +0300

    fixed authorization frontend

--- src/components/Mycard.js
+++ src/components/Mycard.js
@@ -4,9 +4,10 @@ import serg from '../resources/surgeon.jpg'
 
 export default function MyCard({ title, desc, img }) {
     return (
+
         <Card style={{ width: '18rem'}}>
             <Card.Img variant="contain" src={img} />
-            <Card.Body>
+            <Card.Body style={{ background: 'black', color: 'white'}}>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text className="test-rect">{desc}</Card.Text>
             </Card.Body>
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -4,5 +4,6 @@ export const CHAT_ROUTE = '/chat'
 
 export const LOGIN_ROUTE = '/login'
 export const REGISTER_ROUTE = '/register'
+export const PROFILE_ROUTE = '/profile'
 
 export const ROOT_ROUTE = '/'
